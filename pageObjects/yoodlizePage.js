08df0100f9b0a739e8fd51d0703e9232c0ae7703
hachir <ha1991ha@yahoo.com>
2020-03-08 21:25:27 -0600

    yoodlizePage.js

---
+++ pageObjects/yoodlizePage.js
@@ -0,0 +1,39 @@
+module.exports = {
+    url: 'https://alpha.yoodlize.com/',
+    elements: {
+
+        recreationalVehicles: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]',
+            locateStrategy: 'xpath'
+        },
+        llider: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]',
+            locateStrategy: 'xpath'
+        },
+ 
+        mmder: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]',
+            locateStrategy: 'xpath'
+        },
+
+        roror: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]',
+            locateStrategy: 'xpath'
+        },
+
+        goror: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]',
+            locateStrategy: 'xpath'
+        },
+        mkider: {
+            selector: '//div[@class="sc-jKVCRD jSqgxr"]',
+            locateStrategy: 'xpath'
+
+        },
+        roider: {
+            selector: '//img[@src="/images/logo/blueRaw.png"]',
+            locateStrategy: 'xpath'
+        },
+    }
+
+}   
