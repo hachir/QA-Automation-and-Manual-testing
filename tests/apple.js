49c06c558d6a7d82463619b34eee5346531b9a11
hachir <ha1991ha@yahoo.com>
2020-04-04 21:20:22 -0600

    apple.js

--- tests/apple.js
+++ tests/apple.js
@@ -1,10 +1,31 @@
-let  hachir  ={}
-module.exports ={
-    before:browser=> {
-        hachir  = browser.page.reactPage(),
-        hachir .navigate()
+  
+var href
+var fart = {}
+
+module.exports = {
+    before: browser => {
+        fart = browser.page.applePageObjects()
+        fart.navigate()
     },
-    after:browser => {
-        hachir .end()
+    'Search test': browser => {
+        fart
+        .search(fart, "boogerfart")
+        .search(fart, "iPhone")
+        .search(fart, "CODE")
+    },
+    'Apple Sign in Test': browser => {
+        fart
+        .click('@clickBag')
+        .click('@clickSignIn')
+        .setValue('@email', 'chaseqa007@gmail.com')
+        .setValue('@password', 'Jaromandchase007')
+        .click('@signInButton')
+    },
+    
+    'Site Map Test': browser => {
+        fart.useXpath()
+        for (i = 1; i < 51; i++) { //354 if you wanna do all of them; this just tests the first 50
+            fart.testLink(fart, i)
+        }
     }
 }
